3afedb82d548e9bebcd80d92486836f94763b6e6
tamttung <89256402+tamttung@users.noreply.github.com>
2021-10-20 11:10:06 +0900

    Add files via upload

---
+++ chapter3_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <float.h>
+#inclued <limits.h>
+
+
+int main(void)
+{
+	int a = 21000000;
+	int b = 3;
+	
+	int result;
+	
+	result = a*b;
+	printf("%d\n",result);
+	
+	return 0;
+} 
+
+ 
