34ec82a66455f612eb37deaedeeb5ff0147102c5
tamttung <89256402+tamttung@users.noreply.github.com>
2021-10-21 09:56:34 +0900

    Add files via upload

---
+++ chapter4_2.c
@@ -0,0 +1,36 @@
+/*
+	Name: chapter4_2
+	Copyright: tamttung	
+	Author: tamttung
+	Date: 21-10-21 09:21
+	Description: chapter4_2 증감연산자 
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int count, value1, value2;
+	float temperature = 10.5f;
+	
+	count = 10;
+	value1 = ++count;
+	printf("value1 = %d, count = %d\n",value1,count);
+	
+	count = 10;
+	value2 = count++;
+	printf("value2 %d, count = %d\n",value2,count);
+	
+	count = 10;
+	++count;
+	printf("count = %d\n", count);
+	
+	count = 10;
+	count++;
+	printf("count = %d\n", count);
+	
+	temperature++;
+	printf("temperature = %f\n", temperature);
+	
+	return 0;
+}
---
+++ chapter4_3.c
@@ -0,0 +1,23 @@
+//chapter 4_3 관계연산자
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+	int a = 10, b = 20;
+	bool c;
+	
+	c = a < b;
+	
+	if(c==true)
+		printf("111\n");
+		
+	
+	printf("%c\n",c);
+	
+	return 0;
+}
+	
+
+ 
